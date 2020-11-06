fe46c03c7e0e3bfb592683bd9eb5c0aa11c44614
Jonas Terp <jonas.terp@skola.botkyrka.se>
2020-11-06 14:34:21 +0100

    Reworked if-structure

--- src/main.cpp
+++ src/main.cpp
@@ -5,17 +5,17 @@
 const int button = 2;
 const int buzzor = 3;
 const int sensor = 4;
-const int green = 12; // Green light
-const int red = 13; // Red light
+const int green = 12;
+const int red = 13;
 
 // Constants and Variables
 
-int door_open; // Magnetic sensor
-int key_status; // Button
-bool open_allowed = false; // Status
+bool door_open;
+bool key_status;
+bool previously_open = false;
 
 const int blink_time = 375;
-const int check_time = 200;
+const int check_time = 100;
 const int alarm_time = blink_time / 1.3;
 const int alarm_wait = 30;
 bool alarm_confirm = true;
@@ -23,44 +23,23 @@ bool alarm_confirm = true;
 // Functions
 
 void check_key() {
-  key_status = digitalRead(button);
+  if (digitalRead(button) == HIGH) {key_status = true;}
+  else {key_status = false;}
 }
 
 void check_door() {
-  door_open = digitalRead(sensor);
+  if (digitalRead(sensor) == HIGH) {door_open = true;}
+  else {door_open = false;}
 }
 
-void alarm_prompt() {
-  Serial.println("Alarm prompting");
-  for (int i = 0; i < alarm_wait; i++)
-  {
-    Serial.print("Waiting-");
-    tone(buzzor, 523, 500); // C5 4th
-    delay(1000);
-    check_key();
-    if (key_status == HIGH) {
-      Serial.println("Alarm disabled");
-      alarm_confirm = false;
-      tone(buzzor, 523, 250); // C5 4th
-      delay(325);
-      tone(buzzor, 587, 250); // D5 4th
-      delay(325);
-      break;
-    }
-  }
-  if (alarm_confirm == true) {alarmTune();}
-  else {alarm_confirm = true;}
+void lightWrite(int p1, int p2) {
+  digitalWrite(green, p1); // Green Light
+  digitalWrite(red, p2); // Red Light
 }
 
-void greenLight() {
-  digitalWrite(green, HIGH);
-  digitalWrite(red, LOW);
-}
+void greenLight() {lightWrite(HIGH, LOW);}
 
-void redLight() {
-  digitalWrite(red, HIGH);
-  digitalWrite(green, LOW);
-}
+void redLight() {lightWrite(LOW, HIGH);}
 
 void openingTune() {
   Serial.println("Opening Tune");
@@ -80,7 +59,7 @@ void closingTune() {
 
 void alarmTune() {
   Serial.println("Alarm Tune");
-  while (key_status == LOW) {
+  while (key_status == false) {
     redLight();
     
     tone(buzzor, 988, alarm_time); // B5
@@ -97,13 +76,34 @@ void alarmTune() {
   }
 }
 
+void alarm_prompt() {
+  Serial.println("Alarm prompting");
+  for (int i = 0; i < alarm_wait; i++) {
+    Serial.print("Waiting-");
+    tone(buzzor, 523, 500); // C5 4th
+    delay(1000);
+    check_key();
+    if (key_status == true) {
+      Serial.println("Alarm disabled");
+      alarm_confirm = false;
+      tone(buzzor, 523, 250); // C5 4th
+      delay(325);
+      tone(buzzor, 587, 250); // D5 4th
+      delay(325);
+      break;
+    }
+  }
+  if (alarm_confirm == true) {alarmTune();}
+  else {alarm_confirm = true;}
+}
+
 void setup() {
   // put your setup code here, to run once:
   pinMode(button, INPUT);
-  pinMode(sensor, INPUT_PULLUP);
-  pinMode(sensor, OUTPUT);
   pinMode(buzzor, INPUT);
   pinMode(buzzor, OUTPUT);
+  pinMode(sensor, INPUT_PULLUP);
+  pinMode(sensor, OUTPUT);
   pinMode(green, OUTPUT);
   pinMode(red, OUTPUT);
 
@@ -111,50 +111,24 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   check_door();
   check_key();
-  
-  if (door_open == HIGH) { // Door is open
-    if (key_status == HIGH) { // Button is pressed
-      if (open_allowed == true) { // Already open
-        greenLight();
-      }
-      else /* open_allowed == false */ { // Door has been opened
-        greenLight();
-        openingTune();
-        open_allowed = true;
-      }
+  if (key_status == true) {greenLight();
+    if (door_open == true) {
+      if (previously_open == true) {}
+      else {openingTune();previously_open = true;}
     }
-    else /* key_status == LOW */ { // Button is not pressed
-      if (open_allowed == true) { // Door stayed open
-        greenLight();
-      }
-      else /* open_allowed == false */ { // Door has been breached
-        redLight();
-        alarm_prompt();
-      }    
+    else {
+      if (previously_open == true) {redLight();closingTune();previously_open = false;}
     }
   }
-  else /* door_open == false */ { // Door is closed
-    if (key_status == HIGH) { // Button is pressed
-      if (open_allowed == true) { // Door is unlocked
-        greenLight();
-      }
-      else /* open_allowed == false */ { // Door has been unlocked
-        greenLight();
-        open_allowed = true;
-      }
+  else {redLight();
+    if (door_open == true) {
+      if (previously_open == true) {greenLight();}
+      else {alarm_prompt();previously_open = true;}
     }
-    else /* key_status == LOW */ { // Button is not pressed
-      if (open_allowed == true) { // Door has been closed
-        redLight();
-        closingTune();
-        open_allowed = false;
-      }
-      else /* open_allowed == false */ { // Door is larmed
-        redLight();
-      }
+    else {
+      if (previously_open == true) {closingTune();previously_open = false;}
     }
   }
   delay(check_time);
